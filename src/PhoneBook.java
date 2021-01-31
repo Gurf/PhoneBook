763600b9e345971ad7e7fa588f7e4ef199419f0d
Gennadii Ustiuzhinskii <mampormin@gmail.com>
2021-01-31 05:07:28 +0300

    Улучшил вывод в консоль

--- src/PhoneBook.java
+++ src/PhoneBook.java
@@ -9,57 +9,52 @@ public class PhoneBook {
         System.out.println("Введите абонента в формате: фамилия имя отчество!");
         int count = 0;//счетчик числа записей в phoneBookBase
         Scanner scanner = new Scanner(System.in);
-
+        String fioTemp = null;
         boolean isCorrectName = false;
-        boolean isCorrectNumber;
-        while (!isCorrectName) {//надо попробовать убрать while!-не получится
+        boolean isCorrectNumber = false;
+        while (!isCorrectName) {
             String name = scanner.nextLine(); //Считывает строку из System.in
+
             isCorrectName = checkName(name);
             if (!isCorrectName) {
                 System.out.println("Введите корректное имя!");
             } else {
-                String fioTemp = formatName(name);
-//                name = "";
+                fioTemp = formatName(name);
 //                System.out.println(fioTemp);
 
                 for (int i = 0; i <= count; i++) {
                     if (fioTemp.equals(phoneBookBase[i][0])) {
                         list(phoneBookBase, i);
                         isCorrectName = false;
-
                         break;
 
                     } else if (i == count) {
-
-                        System.out.println("Новая запись, введите номер телефона!");
+                        System.out.println(fioTemp);
+                        System.out.println("Новая запись, введите номер телефона (11 цифр)!");
                         isCorrectNumber = false;
-
-                        while (!isCorrectNumber) {
-                            String number = scanner.nextLine(); //Считывает строку из System.in
-                            isCorrectNumber = checkNumber(number);
-                            if (!isCorrectNumber) {
-                                System.out.println("Введите корректный номер!");
-                            } else {
-                                String numberTemp = formatNumber(number);
-                                System.out.println(numberTemp);
-                                add(phoneBookBase, fioTemp, numberTemp, count);
-                                count++;
-                                isCorrectName = false;
-                            }
-                        }
-                        break;
                     }
                 }
-
-
+            }
+            while (!isCorrectNumber) {
+                String number = scanner.nextLine(); //Считывает строку из System.in
+                isCorrectNumber = checkNumber(number);
+                if (!isCorrectNumber) {
+                    System.out.println("Введите корректный номер!");
+                } else {
+                    String numberTemp = formatNumber(number);
+                    System.out.println("Сохранено: " + numberTemp);
+                    add(phoneBookBase, fioTemp, numberTemp, count);
+                    count++;//пинаем счетчик
+                    isCorrectName = false;
+                }
             }
         }
     }
 
+    //======М Е Т О Д Ы===================================================
     public static boolean checkName(String name) {
         String[] partOfName = name.trim().split(" ");
         return partOfName.length == 3;
-//        return true;
     }
 
     public static String formatName(String name) {
@@ -67,20 +62,18 @@ public class PhoneBook {
         for (int i = 0; i < fio.length; i++) {
             fio[i] = fio[i].replaceFirst(fio[i].substring(0, 1), fio[i].substring(0, 1).toUpperCase());
         }
-//        sortByLength(fio);
+//        sortByLength(fio);Убрал т.к. не прикольно когда фамилия имя и отчество могут встать на разные места
         String newFIO = fio[0];
         for (int i = 1; i < fio.length; i++) {
             newFIO = newFIO + " " + fio[i];
         }
 
         return newFIO;
-//        return "";
     }
 
     public static boolean checkNumber(String phoneNumber) {
         String clean = phoneNumber.replaceAll("[^0-9]", "");
         return clean.length() == 11;
-//        return true;
     }
 
     public static String formatNumber(String telephoneN) {
@@ -89,17 +82,16 @@ public class PhoneBook {
         String output = "+7 " + clean.substring(1, 4) + " " + clean.substring(4, 7) + " " + clean.substring(7, 9) + " " + clean.substring(9, 11);
 
         return output;
-//        return "";
     }
 
     //добавляем новую запись в телефонную книгу
     public static void add(String[][] book, String name, String number, int index) {
-        book[index][0] = name;//add logic
-        book[index][1] = number;//add logic
+        book[index][0] = name;
+        book[index][1] = number;
     }
 
-    //выводим номер телефона, если ФИО абонента уже имеется в базе
+    //выводим ФИО и номер телефона, если ФИО абонента уже имеется в базе
     public static void list(String[][] book, int index) {
-        System.out.println(book[index][1]);//print phone book
+        System.out.println(book[index][0] + ": " + book[index][1]);
     }
 }
