733b3658e2a48c3937169e73edd6a2d054229d12
Gennadii Ustiuzhinskii <mampormin@gmail.com>
2021-02-02 16:53:49 +0300

    Попытался копирование массива в новый при переполнении старого вынести в отдельный метод (не работает)

--- src/PhoneBook.java
+++ src/PhoneBook.java
@@ -47,24 +47,37 @@ public class PhoneBook {
                     count++;//пинаем счетчик
 //                    System.out.println(phoneBookBase.length);
 //                    System.out.println(count);
-//                    System.out.println(Arrays.deepToString(phoneBookBase));//ВЫводим на печать массив
+                    System.out.println(Arrays.deepToString(phoneBookBase));//ВЫводим на печать массив
 
                     isCorrectName = false;
                 }
             }
-            //увеличиваем длину имеющегося массива на половину при его наполнении и инициализируем пустыми строками
-            if (count == phoneBookBase.length) {
 
-                phoneBookBase = Arrays.copyOf(phoneBookBase, phoneBookBase.length + phoneBookBase.length / 2);
+            //увеличиваем длину  массива на половину при его наполнении и инициализируем "свободный хвост массива" в цикле for
+            if (count == phoneBookBase.length) { //если количество записей равно теущей длине массива
+                newLengthArr(phoneBookBase);//c вызовом этого метода не работет
+
+                //то увеличиваем длину массива
+                /*phoneBookBase = Arrays.copyOf(phoneBookBase, phoneBookBase.length + phoneBookBase.length / 2);
                 for (int i = count; i < phoneBookBase.length; i++) {
                     phoneBookBase[i] = new String[2];
-                }
-//                System.out.println(Arrays.deepToString(phoneBookBase));//ВЫводим на печать массив
+                }*/
+
+                System.out.println(Arrays.deepToString(phoneBookBase));//ВЫводим на печать массив
             }
         }
     }
 
     //======М Е Т О Д Ы===================================================
+    //увеличиваем длину массива
+    public static void newLengthArr(String[][] arr) {
+        int oldLength = arr.length;
+        arr = Arrays.copyOf(arr, arr.length + arr.length / 2);
+        for (int i = oldLength; i < arr.length; i++) {
+            arr[i] = new String[2];
+        }
+    }
+
     public static boolean checkName(String name) {
         String[] partOfName = name.trim().split(" ");
         return partOfName.length == 3;
@@ -86,13 +99,13 @@ public class PhoneBook {
 
     public static boolean checkNumber(String phoneNumber) {
         String clean = phoneNumber.replaceAll("[^0-9]", "");
-        return clean.length() == 11;
+        return clean.length() == 2;
     }
 
     public static String formatNumber(String telephoneN) {
         String clean = telephoneN.replaceAll("[^0-9]", "");
 
-        String output = "+7 " + clean.substring(1, 4) + " " + clean.substring(4, 7) + " " + clean.substring(7, 9) + " " + clean.substring(9, 11);
+        String output = "+7 " + clean/*clean.substring(1, 4) + " " + clean.substring(4, 7) + " " + clean.substring(7, 9) + " " + clean.substring(9, 11)*/;
 
         return output;
     }
